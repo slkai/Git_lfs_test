ae4dd6d69e29a335e7a012cb81eda872265c7905
Alan <alandeng@mooyoo.com.cn>
2016-10-20 20:53:04 +0800

    init

---
+++ Git_lfs_test/Git_lfs_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git_lfs_test
+//
+//  Created by Alan on 16/10/20.
+//  Copyright © 2016年 Alan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
